b75b94523699473dbda7a603366dceef9718b91f
Mohak Chauhan <mohak46812@gmail.com>
2024-02-16 21:19:58 +0530

    Removed a line

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react'
-import ProfilePosts from '../components/ProfilePosts'
 import { Link, useLocation } from 'react-router-dom';
 import { UserContext } from '../context/Usercontext';
 import axios from 'axios';
